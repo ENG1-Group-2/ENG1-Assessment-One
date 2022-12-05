626b4bdd00a15941779c0f90bffe2d4ad9484160
Vivek <vm800@york.ac.uk>
2022-12-05 15:24:04 +0000

    The map does not seem to fit the full window - too zoomed in

--- JavaDev/core/src/com/mygdx/game/PiazzaPanicGame.java
+++ JavaDev/core/src/com/mygdx/game/PiazzaPanicGame.java
@@ -2,19 +2,19 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-public class PiazzaPanicGame extends ApplicationAdapter implements InputProcessor {
+public class PiazzaPanicGame extends ApplicationAdapter{
 	TiledMap tiledMap;
-	OrthographicCamera camera;
+	PerspectiveCamera camera;
 	TiledMapRenderer tiledMapRenderer;
+	FitViewport viewport;
 
 
 	@Override
@@ -25,19 +25,19 @@ public class PiazzaPanicGame extends ApplicationAdapter implements InputProcesso
 		float width = Gdx.graphics.getWidth();
 		float height = Gdx.graphics.getHeight();
 
-		camera = new OrthographicCamera();
 
-		camera.setToOrtho(false, width, height);
+		camera = new PerspectiveCamera();
+		viewport = new FitViewport(width, height, camera);
+
+
 		camera.update();
 
 		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render () {
 		camera.update();
-		tiledMapRenderer.setView(camera);
 		tiledMapRenderer.render();
 	}
 
@@ -46,56 +46,7 @@ public class PiazzaPanicGame extends ApplicationAdapter implements InputProcesso
 		tiledMap.dispose();
 	}
 
-	@Override
-	public boolean keyDown(int keycode) {
-		return false;
+	public void resize(int width, int height) {
+		viewport.update(width, height);
 	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		if(keycode == Input.Keys.LEFT)
-			camera.translate(-32,0);
-		if(keycode == Input.Keys.RIGHT)
-			camera.translate(32,0);
-		if(keycode == Input.Keys.UP)
-			camera.translate(0,-32);
-		if(keycode == Input.Keys.DOWN)
-			camera.translate(0,32);
-		if(keycode == Input.Keys.NUM_1)
-			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
-		if(keycode == Input.Keys.NUM_2)
-			tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(float amountX, float amountY) {
-		return false;
-	}
-
 }
