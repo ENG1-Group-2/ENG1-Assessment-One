e31518170f00b8f7d6eb6718c8c80025fe04e845
Vivek <vm800@york.ac.uk>
2022-12-05 12:50:19 +0000

    File structure changed

--- Tiled/Tiles.tsx
+++ Tiled/Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Tiles" tilewidth="32" tileheight="32" tilecount="35" columns="35">
- <image source="../../Downloads/Spritesheets(1)/Spritesheets/Tiles.png" width="1120" height="32"/>
+ <image source="../../../Downloads/Spritesheets(1)/Spritesheets/Tiles.png" width="1120" height="32"/>
 </tileset>
